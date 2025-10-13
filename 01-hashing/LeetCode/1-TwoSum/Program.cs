4d92daeb4b820155b5be81aa0cf4ef66c06e3718
d-luan <dluan1285@gmail.com>
2025-10-13 14:13:56 -0300

    feat: Add solution for LeetCode problem 'Two Sum'

---
+++ 01-hashing/LeetCode/1-TwoSum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1_TwoSum;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = new int[] { 2, 7, 11, 15 };
+
+        Console.WriteLine("Enter a value:");
+        int target = int.Parse(Console.ReadLine());
+
+        int[] resultIndex = TwoSum(numbers, target);
+
+        if (resultIndex.Length == 2)
+        {
+            Console.WriteLine($"Index found: {resultIndex[0]} and {resultIndex[1]}");
+            Console.WriteLine($"Value pair: {numbers[resultIndex[0]]} and {numbers[resultIndex[1]]}");
+        }
+        else
+        {
+            Console.WriteLine("Not number founder!");
+        }
+    }
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> numIndex = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (numIndex.ContainsKey(complement))
+            {
+                return new int[] { numIndex[complement], i };
+            }
+            numIndex[nums[i]] = i;
+        }
+        return new int[0];
+    }
+}
---
+++ 01-hashing/LeetCode/README.md
@@ -0,0 +1,11 @@
+## LeetCode Problems: Hashing
+
+This directory contains my solutions to LeetCode problems that are effectively solved using hash tables (`Dictionary` in C#).
+
+Each solution demonstrates a practical application of hashing concepts, such as frequency counting or finding complements, to achieve optimal performance.
+
+### Solved Problems
+
+| Problem | Solution | Key Concept Used | Time Complexity |
+| --- | --- | --- | --- |
+| [1. Two Sum](https://leetcode.com/problems/two-sum/) | [C#](./1-TwoSum/Program.cs) | Finding complements in O(1) using Hash Map| **O(n)**
