8ef50705669130e7328b5c4f032dc3b238a7764d
d-luan <dluan1285@gmail.com>
2025-10-14 11:54:50 -0300

    feat(geeksforgeeks): add CheckForDisjoint solution

---
+++ 01-hashing/geeksforgeeks/CheckForDisjoint/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckForDisjoint;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] a = { 12, 34, 11, 9, 3 };
+        int[] b = { 7, 2, 1, 5 };
+
+        if (CheckForDisjoint(a, b))
+        {
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+    }
+
+    public static bool CheckForDisjoint(int[] listOne, int[] listTwo)
+    {
+        HashSet<int> mainSet = new HashSet<int>(listOne);
+
+        foreach (int element in listTwo)
+        {
+            if (mainSet.Contains(element))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
--- 01-hashing/geeksforgeeks/README.md
+++ 01-hashing/geeksforgeeks/README.md
@@ -9,3 +9,4 @@ Each solution demonstrates a practical application of hashing concepts, such as 
 | Problem | Solution | Key Concept Used | Time Complexity |
 | --- | --- | --- | --- |
 | [Check For Subset](https://www.geeksforgeeks.org/dsa/find-whether-an-array-is-subset-of-another-array-set-1/) | [Code](./CheckForSubset/Program.cs) | Using a HashSet for efficient O(1) lookups| **O(m + n)**
+| [Check For Disjoint](https://www.geeksforgeeks.org/dsa/check-two-given-sets-disjoint/) | [Code](./CheckForDisjoint/Program.cs) | Using a HashSet for efficient O(1) lookups| **O(m + n)**
