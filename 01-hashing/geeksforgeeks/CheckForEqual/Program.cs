4d07989bf001ea175f22fd0aa86ab03d9eeadcf4
d-luan <dluan1285@gmail.com>
2025-10-16 16:38:16 -0300

    feat(geeksforgeeks) add CheckForEqual solution

---
+++ 01-hashing/geeksforgeeks/CheckForEqual/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckForEqual;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] listOne = { 1, 2, 5, 4, 0 };
+        int[] listTwo = { 2, 5, 4, 0, 1 };
+
+        if (CheckForEqual(listOne, listTwo))
+        {
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+    }
+
+    public static bool CheckForEqual(int[] listOne, int[] listTwo)
+    {
+        if (listOne.Length != listTwo.Length)
+        {
+            return false;
+        }
+        
+        Dictionary<int, int> frequencyMap = new Dictionary<int, int>();
+
+        foreach (int num in listOne)
+        {
+            if (frequencyMap.ContainsKey(num))
+            {
+                frequencyMap[num]++;
+            }
+            else
+            {
+                frequencyMap[num] = 1;
+            }
+        }
+
+        foreach (int num in listTwo)
+        {
+            if (!frequencyMap.ContainsKey(num) || frequencyMap[num] == 0)
+            {
+                return false;
+            }
+
+            frequencyMap[num]--;
+        }
+        
+        return true;
+    }
+}
--- 01-hashing/geeksforgeeks/README.md
+++ 01-hashing/geeksforgeeks/README.md
@@ -11,3 +11,4 @@ Each solution demonstrates a practical application of hashing concepts, such as 
 | [Check For Subset](https://www.geeksforgeeks.org/dsa/find-whether-an-array-is-subset-of-another-array-set-1/) | [Code](./CheckForSubset/Program.cs) | Using a HashSet for efficient O(1) lookups| **O(m + n)**
 | [Check For Disjoint](https://www.geeksforgeeks.org/dsa/check-two-given-sets-disjoint/) | [Code](./CheckForDisjoint/Program.cs) | Using a HashSet for efficient O(1) lookups| **O(m + n)**
 | [Check For Disjoint (Memory Optimized)](https://www.geeksforgeeks.org/dsa/check-two-given-sets-disjoint/) | [Code](./CheckForDisjointMemoryOptimized/Program.cs) | Memory optimization by hashing the smaller array. | **O(m + n)**
+| [Check For Equal](https://www.geeksforgeeks.org/dsa/check-if-two-arrays-are-equal-or-not/) | [Code](./CheckForEqual/Program.cs) | Using frequency counter in dictionary | **O(m + n)**
