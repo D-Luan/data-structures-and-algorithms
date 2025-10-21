12536c05ebd7b9b62e86d464d7b9084e24611773
d-luan <dluan1285@gmail.com>
2025-10-21 11:41:58 -0300

    feat(02-two-pointers): add PalindromeNumber solution

---
+++ 02-two-pointers/LeetCode/9-PalindromeNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _9_PalindromeNumber;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 121;
+
+        Console.WriteLine((IsPalindrome(x) ? "True" : "False"));
+    }
+
+    public static bool IsPalindrome(int x)
+    {
+        if (x < 0 || (x % 10 == 0 && x != 0))
+        {
+            return false;
+        }
+
+        int reversedNumber = 0;
+
+        while (x > reversedNumber)
+        {
+            reversedNumber = reversedNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == reversedNumber || x == reversedNumber / 10;
+    }
+}
---
+++ 02-two-pointers/LeetCode/README.md
@@ -0,0 +1,9 @@
+## LeetCode Problems: Hashing
+
+This directory contains my solutions to LeetCode problems that are effectively solved using Two Pointers Technique.
+
+### Solved Problems
+
+| Problem | Solution | Key Concept Used | Time Complexity |
+| --- | --- | --- | --- |
+| [9. Palindrome Number](https://leetcode.com/problems/palindrome-number/description/) | [Code](./9-PalindromeNumber/Program.cs) | Using arithmetics operations for reversed number| **O(log10​(x))**
